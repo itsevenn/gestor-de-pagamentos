ddaee839bf7b0a2d4909b8c73624b039afabe23e
everton silva <evertonsilva948@gmail.com>
2025-07-16 14:24:42 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/hooks/use-theme.ts
+++ src/hooks/use-theme.ts
@@ -1,8 +1,6 @@
 
 'use client';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import type { ThemeProviderProps } from 'next-themes/dist/types';
+import { useTheme } from 'next-themes';
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-}
+export { NextThemesProvider as ThemeProvider, useTheme };
